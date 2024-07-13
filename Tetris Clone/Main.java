b273945e9fe69d2df693740201972da539ce2ccc
Evaldas <81704689+32Butterfly@users.noreply.github.com>
2024-07-13 20:08:14 +0300

    Delete not needed files

--- Tetris Clone/Main.java
+++
@@ -1,8 +0,0 @@
-package org.example.tetrisclone;
-
-public class Main {
-    public static void main(String[] args) {
-        // Call the start method of TimeZoneApp class
-        Tetris.launch(Tetris.class, args);
-    }
-}
